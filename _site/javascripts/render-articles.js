5f6ad68ed107ca853ef953c28bec1602f2b3c5f0
AJ Singh <ajsingh273@gmail.com>
2015-11-13 11:35:21 -0500

    home page updates

--- _site/javascripts/render-articles.js
+++ _site/javascripts/render-articles.js
@@ -1,24 +1,23 @@
-<script>
+
                                                 $(document).ready(function() {
-                                                                get_results();
+                                                                get_results2();
                                                 });
-                                                function get_results()
+                                                function get_results2()
                                                 {
-                                                                $.getJSON('https://api.govwizely.com/v2/trade_articles/search?api_key=-DgaM8IefU4280JDV36SHZC1&size=100', function(d)
+                                                                $.getJSON('https://api.govwizely.com/v2/trade_articles/search?api_key=-DgaM8IefU4280JDV36SHZC1&size=5', function(d)
                                                                 {
-                                                                                $("#results_container_articles").empty();
+                                                                                $("#results_container2").empty();
                                                                                 for (x in d.results)
                                                                                 {
-                                                                                                row = '<div class="list-group" id="result_' + x + '"><a href="#" class="list-group-item"> <h4 class="result_seo_metadata_title">' + d.results[x].seo_metadata_title + '</h4><p class="result_seo_metadata_description">' + d.results[x].seo_metadata_description + '</p></a>'
+                                                                                                row = '<div class="box-style-3"><div class="icon-container default-bg"><i class="fa fa-newspaper-o"></i></div><div class="body" id="result_' + x + '"> <h4 class="result_seo_metadata_title text-thin">' + d.results[x].seo_metadata_title + '</h4><h5 class="result_creation_date">' + d.results[x].creation_date +  '</h5><p class="result_seo_metadata_description">' + d.results[x].seo_metadata_description + '</p>' + '<hr></div>'
                                                                                                 if (x < d.results.length-1)
                                                                                                 {
                                                                                                                 row = row ;
                                                                                                 }
                                                                                                 row = row + "</div>";
-                                                                                                $("#results_container_articles").append(row);
-                                                                                                
+                                                                                                $("#results_container2").append(row);
+
                                                                                 }
-                                                                               
+
                                                                 });
                                                 }
-                                </script>
---
+++ javascripts/widget-start.js
@@ -0,0 +1,25 @@
+      $(document).ready(function ($) {
+        $("#example-widget-container").searchWidget({
+          apiKey:   'BieztMWzm1EGY71oPH1KFpwm',
+          host:     'https://api.trade.gov',
+          endpoint: 'consolidated_screening_list'
+        });
+
+        $("#example-trade-leads-widget-container").searchWidget({
+          apiKey:   'BieztMWzm1EGY71oPH1KFpwm',
+          host:     'https://api.trade.gov',
+          endpoint: 'trade_leads'
+        });
+
+        $("#example-trade-events-widget-container").searchWidget({
+          apiKey:   'BieztMWzm1EGY71oPH1KFpwm',
+          host:     'https://api.trade.gov',
+          endpoint: 'trade_events'
+        });
+
+        $("#example-useac-locations-widget-container").searchWidget({
+          apiKey:   'BieztMWzm1EGY71oPH1KFpwm',
+          host:     'https://api.trade.gov',
+          endpoint: 'useac_locations'
+        });
+      });
