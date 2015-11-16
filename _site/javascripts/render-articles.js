b1da21eaac9b06e32719f3e3cb7fef679f757ae5
AJ Singh <ajsingh273@gmail.com>
2015-11-16 07:32:50 -0500

    improved typography 2

--- _site/javascripts/render-articles.js
+++ _site/javascripts/render-articles.js
@@ -9,7 +9,7 @@
                                                                                 $("#results_container2").empty();
                                                                                 for (x in d.results)
                                                                                 {
-                                                                                                row = '<div class="box-style-3"><div class="icon-container default-bg"><i class="fa fa-newspaper-o"></i></div><div class="body" id="result_' + x + '"> <h4 class="result_seo_metadata_title text-thin">' + d.results[x].seo_metadata_title + '</h4><h5 class="result_creation_date">' + d.results[x].creation_date +  '</h5><p class="result_seo_metadata_description">' + d.results[x].seo_metadata_description + '</p>' + '<hr></div>'
+                                                                                                row = '<div class="box-style-3"><div class="icon-container default-bg"><i class="fa fa-newspaper-o"></i></div><div class="body" id="result_' + x + '"> <h4 class="result_seo_metadata_title text-thin">' + d.results[x].seo_metadata_title + '</h4><h7 class="result_creation_date">' + d.results[x].creation_date +  '</h7></br><h7 class="result_seo_metadata_description">' + d.results[x].seo_metadata_description + '</h7>' + '</div>'
                                                                                                 if (x < d.results.length-1)
                                                                                                 {
                                                                                                                 row = row ;
